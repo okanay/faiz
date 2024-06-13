3ac1164cdee1018233a894ccbea2967e02c1b918
okanay <okanay@hotmail.com>
2024-06-13 15:29:58 +0300

    A2

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -4,7 +4,7 @@ import { Result } from "@/components/result";
 export default function Home() {
   return (
     <main className="flex h-fit min-h-svh w-full flex-col justify-center">
-      <section className="mx-auto flex w-full max-w-7xl flex-col items-center justify-center gap-4 md:flex-row md:gap-8">
+      <section className="mx-auto flex w-full max-w-7xl flex-col items-center justify-center gap-4 py-8 md:flex-row md:gap-8">
         <Form />
         <Result />
       </section>
